69a1c62a4cac23f5d8a3befd37e77e6f41b8db09
mobius-eng <Alexey.Cherkaev@gmail.com>
2016-11-08 14:37:51 +0200

    Fix closures - doesnt work

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,3 @@
 cd(joinpath(Pkg.dir("TWPBVP"), "deps"))
 pic = @windows ? "" : "-fPIC"
-run(`gfortran -m$WORD_SIZE -fdefault-real-8 -fdefault-integer-8 -ffixed-form $pic -shared -O3 -o libtwpbvpc.so twpbvpc.f`)
+run(`gfortran -m$WORD_SIZE -fdefault-real-8 -fdefault-integer-8 -ffixed-form $pic -shared -g -o libtwpbvpc.so twpbvpc.f`)
--- src/TWPBVP.jl
+++ src/TWPBVP.jl
@@ -5,6 +5,65 @@ export twpbvpc
 
 const libtwpbvpc = joinpath(Pkg.dir("TWPBVP"), "deps", "libtwpbvpc.so")
 
+immutable TWPBVPCProblem
+	fsub :: Function
+	dfsub :: Function
+	gsub :: Function
+	dgsub :: Function
+end
+
+"""
+Provides the conversion betweeen Julia's function (including closures)
+and Fortran's subroutine. The closure is passed in `rpar` parameter
+that Fortran believes to be an array of Float64, it never looks into it
+anyway and only passes it around.
+"""
+function unsafe_fsub(rn :: Ref{Int64}, rx :: Ref{Float64}, py :: Ptr{Float64}, pdy :: Ptr{Float64}, rpar :: Ptr{Float64}, ipar :: Ptr{Int64}) :: Void
+	x = rx[]
+	n = rn[]
+	y = unsafe_wrap(Array, py, n)
+	dy = unsafe_wrap(Array, pdy, n)
+	problem = unsafe_pointer_to_objref(rpar) :: TWPBVPCProblem
+	problem.fsub(x, y, dy)
+	return nothing
+end
+
+function unsafe_dfsub(rn :: Ref{Int64}, rx :: Ref{Float64}, ry :: Ptr{Float64}, rjac :: Ptr{Float64}, rpar :: Ptr{Float64}, ipar :: Ptr{Int64}) :: Void
+	x = rx[]
+	n = rn[]
+	y = unsafe_wrap(Array, py, n)
+	jac = unsafe_wrap(Array, pjac, (n,n))
+	problem = unsafe_pointer_to_objref(rpar) :: TWPBVPCProblem
+	problem.dfsub(x, y, jac)
+	return nothing
+end
+
+function unsafe_gsub(ri :: Ref{Int64}, rn :: Ref{Int64}, py :: Ptr{Float64}, pg :: Ref{Float64}, rpar :: Ptr{Float64}, ipar :: Ptr{Int64}) :: Void
+	i = ri[]
+	n = rn[]
+	y = unsafe_wrap(Array, py, n)
+	problem = unsafe_pointer_to_objref(rpar) :: TWPBVPCProblem
+	rg[] = problem.gsub(i, y)
+	return nothing
+end
+
+function unsafe_dgsub(ri :: Ref{Int64}, rn :: Ref{Int64}, py :: Ptr{Float64}, pdg :: Ptr{Float64}, rpar :: Ptr{Float64}, ipar :: Ptr{Int64}) :: Void
+	i = ri[]
+	n = rn[]
+	y = unsafe_wrap(Array, py, n)
+	dg = unsafe_wrap(Array, pdg, n)
+	problem = unsafe_pointer_to_objref(rpar) :: TWPBVPCProblem
+	rg[] = problem.dgsub(i, y, dg)
+	return nothing
+end
+
+const fsub_ptr = cfunction(unsafe_fsub, Void, (Ref{Int64}, Ref{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}))
+
+const dfsub_ptr = cfunction(unsafe_dfsub, Void, (Ref{Int64}, Ref{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}))
+
+const gsub_ptr = cfunction(unsafe_gsub, Void, (Ref{Int64}, Ref{Int64}, Ptr{Float64}, Ref{Float64}, Ptr{Float64}, Ptr{Int64}))
+
+const dgsub_ptr = cfunction(unsafe_dgsub, Void, (Ref{Int64}, Ref{Int64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}))
 
 """
 Thin layer of compatibility
@@ -24,32 +83,13 @@ function twpbvpc(nlbc :: Int64,
 		# ipar :: Vector{Int64},
 		iflbvp :: Ref{Int64})
 
-	# As Fortran 77 has no clausers, rpar and ipar are used to capture external
-	# parameters. In Julia: use clausers
-	function fsub_par(n :: Int64, x :: Float64, y :: Vector{Float64}, dy :: Vector{Float64}, rpar :: Vector{Float64}, ipar :: Vector{Float64})
-		fsub(x, y, dy)
-	end
-
-	function dfsub_par(n :: Int64, x :: Float64, y :: Vector{Float64}, jac :: Array{Float64, 2}, rpar :: Vector{Float64}, ipar :: Vector{Int64})
-		dfsub(x, y, jac)
-	end
-
-	function gsub_par(i :: Int64, n :: Int64, u :: Vector{Float64}, g :: Ref{Float64}, rpar :: Vector{Float64}, ipar :: Vector{Int64})
-		g.x = gsub(i, u)
-	end
-
-	function dgsub_par(i :: Int64, n :: Int64, u :: Vector{Float64}, dg :: Vector{Float64}, rpar :: Vector{Float64}, ipar :: Vector{Int64})
-		dgsub(i, u, dg)
-	end
-
-	cf_fsub = cfunction(fsub_par, Void, (Ref{Int64}, Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int64}))
-	cf_dfsub = cfunction(dfsub_par, Void, (Ref{Int64}, Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int64}))
-	cf_gsub = cfunction(gsub_par, Void, (Ref{Int64}, Ref{Int64}, Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int64}))
-	cf_dgsub = cfunction(dgsub_par, Void, (Ref{Int64}, Ref{Int64}, Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int64}))
+	# Keep problem functions in rpar
+	# HACK!
+	rpar = TWPBVPCProblem(fsub, dfsub, gsub, dgsub)
 
 	# Fake external parameters
 	# Can't have it 0-length as it would be Any[0] and not Float64[0]
-	local rpar :: Vector{Float64} = [0.0]
+	# local rpar :: Vector{Float64} = [0.0]
 	local ipar :: Vector{Int64} = [0]
 
 	# No need to pass these parameters
@@ -79,25 +119,25 @@ function twpbvpc(nlbc :: Int64,
 	ccall((:twpbvpc_, libtwpbvpc), Void,
 		(Ref{Int64}, Ref{Int64},					# ncomp, nlbc,
 		Ref{Float64}, Ref{Float64},					# aleft, aright
-		Ref{Int64}, Ref{Float64}, 					# nfxpnt, fixpnt
-		Ref{Int64}, Ref{Int64}, Ref{Float64},		# ntol, ltol, tol
+		Ref{Int64}, Ptr{Float64}, 					# nfxpnt, fixpnt
+		Ref{Int64}, Ptr{Int64}, Ptr{Float64},		# ntol, ltol, tol
 		Ref{Int64}, Ref{Int64}, Ref{Int64},			# linear, givmsh, giveu
 		Ref{Int64}, Ref{Int64},						# nmsh, nxxdim
-		Ref{Float64}, Ref{Int64},					# xx, nudim
-		Ref{Float64}, Ref{Int64},					# u, nmax
-		Ref{Int64}, Ref{Float64},					# lwrkfl, wrk
-		Ref{Int64}, Ref{Int64},						# lwrkin, iwrk
+		Ptr{Float64}, Ref{Int64},					# xx, nudim
+		Ptr{Float64}, Ref{Int64},					# u, nmax
+		Ref{Int64}, Ptr{Float64},					# lwrkfl, wrk
+		Ref{Int64}, Ptr{Int64},						# lwrkin, iwrk
 		Ptr{Void}, Ptr{Void}, Ptr{Void}, Ptr{Void},	# fsub, dfsub, gsub, dgsub
 		Ref{Float64}, Ref{Float64},					# ckappa1, gamma1
-		Ref{Float64}, Ref{Float64}, Ref{Int64},		# ckappa, rpar, ipar
+		Ref{Float64}, Ptr{Void}, Ptr{Int64},		# ckappa, rpar, ipar
 		Ref{Int64}),								# iflbvp
 		ncomp, nlbc, aleft, aright,
 		nfxpnt, fixpnt_v, ntol, ltol, tol,
 		linear, givmsh, giveu, nmsh,
 		nxxdim, xx, nucol, u, nmax,					# nudim = nucol
 		lwrkfl, wrk, lwrkin, iwrk,
-		cf_fsub, cf_dfsub, cf_gsub, cf_dgsub,
-		ckappa1,gamma1,ckappa,rpar,ipar,iflbvp)
+		fsub_ptr, dfsub_ptr, gsub_ptr, dgsub_ptr,
+		ckappa1,gamma1,ckappa,pointer_from_objref(rpar),ipar,iflbvp)
 end
 
 end # module
